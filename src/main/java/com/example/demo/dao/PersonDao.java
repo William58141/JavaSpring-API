24014d3a572d9fcc7795dc6100652459e6b4e457
William Fosmark Haugland <31375260+William58141@users.noreply.github.com>
2022-06-28 02:43:08 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+
+public interface PersonDao {
+
+    int insertPerson (UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+}
